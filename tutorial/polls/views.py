076e500a137237afaf728b82298cea9d180c0540
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-07-31 15:32:23 -0500

    implement results view and template

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -20,7 +20,13 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f'You\'re looking at the results of question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(
+        request,
+        'polls/results.html',
+        { 'question': question }
+    )
 
 
 def vote(request, question_id):
@@ -38,6 +44,5 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(
-            reverse('polls:results',
-            args=(question.id)
-        ))
+            reverse('polls:results', args=(question.id,))
+        )
